703c658231dc1ab1b2a08be3c504d4008c4f9973
Abhi5ingh <abhisteak@gmail.com>
2025-02-25 16:10:12 +0530

    focal loss implemented,corrected batch handling

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,11 +8,12 @@ from keras import layers, models, optimizers
 from src.utils import ensure_dir
 from src.components.model import build_model
 from focal_loss import BinaryFocalLoss
+import math
 
 def train_cnn(dataset_dir="dataset",
               img_size=(224,224),
               batch_size=32,epochs=20,
-              model_save_path="cnn_tki_classifier"):
+              model_save_path="cnn_tki_classifier.keras"):
     """Trains a CNN model on images in dataset_dir and saves the model to model_save_path."""
     logging.info(f"Training CNN model on images in {dataset_dir}")
     if not os.path.exists(dataset_dir):
@@ -46,14 +47,14 @@ def train_cnn(dataset_dir="dataset",
     model.compile(optimizers.Adam(learning_rate=0.001),loss=BinaryFocalLoss(gamma=2.0),metrics=['accuracy'])
 
     model.summary(print_fn=logging.info)
-    class_weight={0:1.0, 1:1.5}
+    class_weight={0:1.0, 1:20}
     history=model.fit(train_generator,
-                      steps_per_epoch=train_generator.samples//batch_size,
+                      steps_per_epoch=math.ceil(train_generator.samples/batch_size),
                       validation_data=val_generator,
-                      validation_steps=val_generator.samples//batch_size,
+                      validation_steps=math.ceil(val_generator.samples/batch_size),
                       class_weight=class_weight,
                       epochs=epochs)
-    model.save(model_save_path,save_format='tf')
+    model.save(model_save_path)
     logging.info(f"Model saved to {model_save_path}")
 
     return model
